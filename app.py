642f5443e205bd0250f25498d1f43a9261efc15a
Onelots <onelots@onelots.fr>
2025-03-03 12:29:29 +0100

    website: 404: introduce 404 page
    Signed-off-by: Onelots <onelots@onelots.fr>

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ def hello_world():  # put application's code here
     # return index.html
     return render_template('index.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run()
